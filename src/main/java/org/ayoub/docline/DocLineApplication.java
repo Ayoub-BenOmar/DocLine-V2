5d7e5c6f758d7429360e0c03b47f07738c2fabcb
ayuub <ayoubbenomar1230@gmail.com>
2026-01-16 11:35:36 +0100

    First commit and initialized the project structure

---
+++ src/main/java/org/ayoub/docline/DocLineApplication.java
@@ -0,0 +1,13 @@
+package org.ayoub.docline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocLineApplication.class, args);
+    }
+
+}
