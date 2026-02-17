742d96f065d7beecd5cff273ae5bda4df0e953ed
Ayoub Ben Omar <ayoubbenomar123@gmail.com>
2026-02-17 20:11:20 +0000

    Add CityDto and SpecialtyDto for public data

---
+++ Backend/src/main/java/org/ayoub/docline/model/dto/CityDto.java
@@ -0,0 +1,15 @@
+package org.ayoub.docline.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityDto {
+    private Integer id;
+    private String cityName;
+}
