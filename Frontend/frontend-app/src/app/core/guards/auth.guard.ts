146eeda68b00466c16eb7855f1b29f3f26524308
Ayoub Ben Omar <ayoubbenomar123@gmail.com>
2026-03-09 13:47:43 +0000

    feat(security): implement RBAC in AuthGuard - Add checking for required roles in route data - Implement role-based redirection to avoid 403 errors and loops - Redirect unauthorized users to their specific dashboard instead of login

--- Frontend/frontend-app/src/app/core/guards/auth.guard.ts
+++ Frontend/frontend-app/src/app/core/guards/auth.guard.ts
@@ -2,11 +2,28 @@ import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
     const router = inject(Router);
 
     if (authService.isAuthenticated()) {
+        const userRole = authService.getUserRole();
+        const expectedRoles = route.data['roles'] as Array<string>;
+
+        if (expectedRoles && expectedRoles.length > 0) {
+            if (userRole && expectedRoles.includes(userRole)) {
+                return true;
+            } else {
+                if (userRole === 'ROLE_ADMIN') {
+                    router.navigate(['/admin/dashboard']);
+                } else if (userRole === 'ROLE_DOCTOR') {
+                    router.navigate(['/doctor/dashboard']);
+                } else {
+                    router.navigate(['/']);
+                }
+                return false;
+            }
+        }
         return true;
     } else {
         router.navigate(['/login']);
