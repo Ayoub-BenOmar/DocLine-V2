527c347c9543f32b605537d934bbc29baad67be0
Ayoub Ben Omar <ayoubbenomar123@gmail.com>
2026-03-24 11:18:28 +0100

    enhance homepage layout with responsive design and updated navbar

--- Frontend/frontend-app/src/app/features/public/home/home.component.ts
+++ Frontend/frontend-app/src/app/features/public/home/home.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule, RouterModule, NavbarComponent],
+    imports: [CommonModule, RouterModule, NavbarComponent, FooterComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
-export class HomeComponent {
-}
-
+export class HomeComponent {}
